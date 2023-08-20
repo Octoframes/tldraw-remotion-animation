b050150c43548381f12eab299d63ad7adc7a597f
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-20 18:44:53 +0200

    tweaks

--- src/Overlay.tsx
+++ src/Overlay.tsx
@@ -1,12 +1,6 @@
-import {
-	AbsoluteFill,
-	interpolate,
-	spring,
-	useCurrentFrame,
-	useVideoConfig,
-} from 'remotion';
-import React, {useMemo} from 'react';
-import {TDShapeType, Tldraw, TldrawApp} from '@tldraw/tldraw';
+import { AbsoluteFill, interpolate,spring, useCurrentFrame, useVideoConfig } from 'remotion';
+import React, { useMemo } from 'react';
+import { TDShapeType, Tldraw, TldrawApp } from '@tldraw/tldraw';
 
 const disappearBeforeEnd = 20;
 
@@ -16,27 +10,16 @@ export const Overlay: React.FC = () => {
 	const frame = useCurrentFrame();
 	const {fps, durationInFrames} = useVideoConfig();
 
-	const scale = spring({
-		fps,
-		from: 0.8,
-		to: 1,
-		frame,
-		config: {
-			mass: 0.5,
-		},
-	});
-
 	const manipulateshape = spring({
 		fps,
 		from: 0,
 		to: 100,
-		frame: frame - 0.3 * fps,
+		frame: frame,
 		config: {
 			mass: 0.5,
 		},
 		durationInFrames: 40,
 	});
-	console.log(frame - 0.3 * fps);
 
 	const handleMount = React.useCallback((app: TldrawApp) => {
 		setApp(app);
@@ -63,7 +46,7 @@ export const Overlay: React.FC = () => {
 	});
 
 	const rotate = interpolate(out, [0, 1], [0, -Math.PI / 20]);
-	const outY = interpolate(out, [0, 1], [0, -1300]);
+	const outY = interpolate(out, [0, 1], [0, -100]);
 
 	const container: React.CSSProperties = useMemo(() => {
 		return {
@@ -74,18 +57,14 @@ export const Overlay: React.FC = () => {
 			height: 400,
 			left: 400,
 			top: 400,
-			scale: String(scale * 2),
-			translate: `0 ${outY}px`,
-			rotate: `${rotate}rad`,
+			transform: `translate(0, ${outY}px) rotate(${rotate}rad) scale(2)`,
 		};
-	}, [scale, outY, rotate]);
+	}, [outY, rotate]);
 
 	return (
 		<AbsoluteFill>
 			<div style={container}>
 				<Tldraw onMount={handleMount} />
-				{/* <div style={title}>Look</div> */}
-				{/* <div style={text}>I'm an overlay!</div> */}
 			</div>
 		</AbsoluteFill>
 	);
