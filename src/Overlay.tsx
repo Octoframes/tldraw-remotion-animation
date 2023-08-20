bbbde952c1c19cce38fe3cbbbb2627a7c0b9ebdd
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-20 18:35:23 +0200

    more tweaks

--- src/Overlay.tsx
+++ src/Overlay.tsx
@@ -5,11 +5,9 @@ import {
 	useCurrentFrame,
 	useVideoConfig,
 } from 'remotion';
-import React, {useMemo} from 'react';
+import React, {useEffect, useMemo} from 'react';
 import {loadFont} from '@remotion/google-fonts/Roboto';
-import { Tldraw } from "@tldraw/tldraw";
-
-
+import {TDShapeType, Tldraw, TldrawApp} from '@tldraw/tldraw';
 
 const {fontFamily} = loadFont();
 
@@ -29,17 +27,48 @@ const text: React.CSSProperties = {
 const disappearBeforeEnd = 20;
 
 export const Overlay: React.FC = () => {
+	const [points] = React.useState();
+	const [app, setApp] = React.useState();
+
 	const frame = useCurrentFrame();
 	const {fps, durationInFrames} = useVideoConfig();
 
 	const scale = spring({
 		fps,
+		from: 0.8,
+		to: 1,
 		frame,
 		config: {
 			mass: 0.5,
 		},
 	});
 
+	const manipulateshape = spring({
+		fps,
+		from: 0,
+		to: 100,
+		frame: frame - 0.3 * fps,
+		config: {
+			mass: 0.5,
+		},
+		durationInFrames: 40,
+	});
+	console.log(frame - 0.3 * fps);
+
+	const handleMount = React.useCallback((app: TldrawApp) => {
+		setApp(app);
+	}, []);
+
+	React.useEffect(() => {
+		if (app) {
+			app.createShapes({
+				id: 'rect1',
+				type: TDShapeType.Rectangle,
+				point: [100, 100 + 0.5 * manipulateshape],
+				size: [400 - manipulateshape, 100 + manipulateshape],
+			});
+		}
+	}, [manipulateshape, app]);
 
 	const out = spring({
 		fps,
@@ -53,19 +82,16 @@ export const Overlay: React.FC = () => {
 	const rotate = interpolate(out, [0, 1], [0, -Math.PI / 20]);
 	const outY = interpolate(out, [0, 1], [0, -1300]);
 
-
-	console.log(outY)
-
 	const container: React.CSSProperties = useMemo(() => {
 		return {
 			position: 'absolute',
 			backgroundColor: 'white',
 			borderRadius: 0,
 			width: 600,
-			height : 400,
+			height: 400,
 			left: 400,
 			top: 400,
-			scale: String(scale*2),
+			scale: String(scale * 2),
 			translate: `0 ${outY}px`,
 			rotate: `${rotate}rad`,
 		};
@@ -74,12 +100,10 @@ export const Overlay: React.FC = () => {
 	return (
 		<AbsoluteFill>
 			<div style={container}>
-			<Tldraw />
+				<Tldraw onMount={handleMount} />
 				{/* <div style={title}>Look</div> */}
 				{/* <div style={text}>I'm an overlay!</div> */}
-
 			</div>
 		</AbsoluteFill>
 	);
 };
-
