cf879b6e53d8aa9824ed48175a8be31dac1cc986
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-03-22 18:36:44 +0100

    add arrow example

--- src/Overlay.tsx
+++ src/Overlay.tsx
@@ -16,24 +16,59 @@ export const Overlay: React.FC = () => {
 	const handleMount = (editor) => {
 		setEditorInstance(editor); // Store the editor instance for later use
 
-		const rectangleId = createShapeId('rectangle');
-
+    const rectangleId1 = createShapeId("rectangle1");
+    const rectangleId2 = createShapeId("rectangle2");
+    const arrowId = createShapeId("arrow");
 		editor.createShapes([
-			{
-				id: rectangleId,
-				type: 'geo',
-				x: 300,
-				y: 300,
-				props: {
-					geo: 'rectangle',
-					w: 150,
-					h: 75,
-					dash: 'draw',
-					color: 'blue',
-					size: 'xl',
-				},
-			},
-		]);
+      {
+        id: rectangleId1,
+        type: "geo",
+        x: 300,
+        y: 300,
+        props: {
+          geo: "rectangle",
+          w: 150,
+          h: 75,
+          dash: "draw",
+          color: "blue",
+          size: "xl",
+        },
+      },
+      {
+        id: rectangleId2,
+        type: "geo",
+        x: 500,
+        y: 500,
+        props: {
+          geo: "rectangle",
+          w: 100,
+          h: 100,
+          dash: "draw",
+          color: "yellow",
+          size: "m",
+        },
+      },
+      {
+        id: arrowId,
+        type: "arrow",
+        props: {
+          start: {
+            type: "binding",
+            boundShapeId: rectangleId1,
+            normalizedAnchor: { x: 0.5, y: 0.5 },
+            isExact: false,
+            isPrecise: true,
+          },
+          end: {
+            type: "binding",
+            boundShapeId: rectangleId2,
+            normalizedAnchor: { x: 0.5, y: 0.5 },
+            isExact: false,
+            isPrecise: true,
+          },
+        },
+      },
+    ]);
 	};
 
 	const xCoordinate = interpolate(frame, [0, durationInFrames], [0, 1000], {
@@ -45,7 +80,7 @@ export const Overlay: React.FC = () => {
 		if (editorInstance) {
 			editorInstance.updateShapes([
 				{
-					id: createShapeId('rectangle'),
+					id: createShapeId('rectangle1'),
 					x: xCoordinate,
 				},
 			]);
